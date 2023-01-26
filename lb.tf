1025b8ac615c99ddc808881016c6f72a13506018
Denise Perez <denise@weka.io>
2023-01-26 14:55:13 +0200

    feat: create vmss

--- lb.tf
+++ lb.tf
@@ -1,5 +1,5 @@
 locals {
-  private_ips_list = azurerm_linux_virtual_machine.vm.*.private_ip_address
+ # private_ips_list = azurerm_linux_virtual_machine.vm.*.private_ip_address
 }
 
 # ================= ui lb =========================== #
@@ -25,7 +25,8 @@ resource "azurerm_lb_backend_address_pool" "ui_lb_backend_pool" {
 resource "azurerm_lb_probe" "ui_lb_probe" {
   loadbalancer_id     = azurerm_lb.ui_lb.id
   name                = "${var.prefix}-${var.cluster_name}-ui-lb-probe"
-  protocol            = "Tcp"
+  protocol            = "Https"
+  request_path        = "/api/v2/healthcheck"
   port                = 14000
   interval_in_seconds = 5
   number_of_probes    = 2
@@ -42,14 +43,14 @@ resource "azurerm_lb_rule" "ui_lb_rule" {
   probe_id                       = azurerm_lb_probe.ui_lb_probe.id
 }
 
-resource "azurerm_lb_backend_address_pool_address" "ui_lb_backend_address_pool" {
-  count                   = length(local.private_ips_list)
-  name                    = "${var.prefix}-${var.cluster_name}-address-pool-${count.index}"
-  backend_address_pool_id = azurerm_lb_backend_address_pool.ui_lb_backend_pool.id
-  virtual_network_id      = data.azurerm_virtual_network.vnet.id
-  ip_address              = local.private_ips_list[count.index]
-  depends_on              = [azurerm_linux_virtual_machine.vm,azurerm_lb_backend_address_pool.ui_lb_backend_pool]
-}
+#resource "azurerm_lb_backend_address_pool_address" "ui_lb_backend_address_pool" {
+ # count                   = length(local.private_ips_list)
+  #name                    = "${var.prefix}-${var.cluster_name}-address-pool-${count.index}"
+  #backend_address_pool_id = azurerm_lb_backend_address_pool.ui_lb_backend_pool.id
+  #virtual_network_id      = data.azurerm_virtual_network.vnet.id
+  #ip_address              = local.private_ips_list[count.index]
+  #depends_on              = [azurerm_linux_virtual_machine.vm,azurerm_lb_backend_address_pool.ui_lb_backend_pool]
+#}
 
 # ================= backend lb =========================== #
 resource "azurerm_lb" "backend-lb" {
@@ -74,7 +75,8 @@ resource "azurerm_lb_backend_address_pool" "lb_backend_pool" {
 resource "azurerm_lb_probe" "backend_lb_probe" {
   loadbalancer_id     = azurerm_lb.backend-lb.id
   name                = "${var.prefix}-${var.cluster_name}-lb-probe"
-  protocol            = "Tcp"
+  protocol            = "Https"
+  request_path        = "/api/v2/healthcheck"
   port                = 14000
   interval_in_seconds = 5
   number_of_probes    = 2
@@ -91,16 +93,26 @@ resource "azurerm_lb_rule" "backend_lb_rule" {
   probe_id                       = azurerm_lb_probe.backend_lb_probe.id
 }
 
-resource "azurerm_lb_backend_address_pool_address" "backend_lb_backend_address_pool" {
-  count                   = length(local.private_ips_list)
-  name                    = "${var.prefix}-${var.cluster_name}-backend-address-pool-${count.index}"
-  backend_address_pool_id = azurerm_lb_backend_address_pool.lb_backend_pool.id
-  virtual_network_id      = data.azurerm_virtual_network.vnet.id
-  ip_address              = local.private_ips_list[count.index]
-  depends_on              = [azurerm_linux_virtual_machine.vm,azurerm_lb_backend_address_pool.lb_backend_pool]
-}
+#resource "azurerm_lb_backend_address_pool_address" "backend_lb_backend_address_pool" {
+ # count                   = length(local.private_ips_list)
+ #name                    = "${var.prefix}-${var.cluster_name}-backend-address-pool-${count.index}"
+  #backend_address_pool_id = azurerm_lb_backend_address_pool.lb_backend_pool.id
+  #virtual_network_id      = data.azurerm_virtual_network.vnet.id
+  #ip_address              = local.private_ips_list[count.index]
+  #depends_on              = [azurerm_linux_virtual_machine.vm,azurerm_lb_backend_address_pool.lb_backend_pool]
+#}
 
 # ================== Private DNS  records ========================= #
+#resource "azurerm_private_dns_a_record" "dns_a_record_ui_lb" {
+#  name                = lower("${var.cluster_name}-ui-check")
+#  zone_name           = var.private_dns_zone_name
+#  resource_group_name = var.rg_name
+#  ttl                 = 300
+#  records             = local.private_ips_list
+#  tags                = merge(var.tags_map, {"weka_cluster": var.cluster_name})
+#  depends_on          = [azurerm_lb.ui_lb]
+#}
+
 resource "azurerm_private_dns_a_record" "dns_a_record_backend_lb" {
   name                = lower("${var.cluster_name}-backend")
   zone_name           = var.private_dns_zone_name
--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,9 @@
-output "ip" {
-  value = local.names_and_ips
-  description = "The IP addresses list of the virtual machines."
-}
-
 output "ssh-key-files-path" {
   value = var.ssh_public_key == null ? "${local.ssh_path}-public-key.pub, ${local.ssh_path}-private-key.pem" : "${var.ssh_public_key} , ${var.ssh_private_key}"
   description = "Displays the path of the ssh keys."
 }
 
 locals {
-  names_and_ips = var.private_network ? [for i in range(var.cluster_size): "${local.vm_names[i]}: ${azurerm_linux_virtual_machine.vm[i].private_ip_address}"] : [for i in range(var.cluster_size): "${local.vm_names[i]}: ${azurerm_public_ip.vm-ip[i].ip_address}"]
   key_vault_name = var.ssh_public_key == null ? azurerm_key_vault.key_vault.name : ""
   blob_commands =<<EOT
 ########################################## download ssh keys command from blob ###########################################################
@@ -31,4 +25,20 @@ function_key=$(az functionapp keys list --name ${azurerm_linux_function_app.func
 curl https://${var.prefix}-${var.cluster_name}-function-app.azurewebsites.net/api/status?code=$function_key
 EOT
   description = "A command to get the cluster status."
+}
+
+output "get-vms-private-ips" {
+  value =<<EOT
+################################## get private ips ######################################################
+  az vmss nic list -g ${var.rg_name} --vmss-name ${azurerm_linux_virtual_machine_scale_set.vmss.name} --subscription ${var.subscription_id} --query "[].ipConfigurations[].privateIpAddress"
+EOT
+  description = "Get private ips of vms"
+}
+
+output "get-vms-public-ips" {
+  value =<<EOT
+################################## get public ips ######################################################
+  az vmss list-instance-public-ips -g ${var.rg_name} --name ${azurerm_linux_virtual_machine_scale_set.vmss.name} --subscription ${var.subscription_id} --query "[].ipAddress"
+EOT
+  description = "Get public ips of vms"
 }
