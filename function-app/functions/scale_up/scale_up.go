5db0eaf093fc245f4d6205c0bce57ceca3350bc7
Assaf Giladi <assaf@weka.io>
2023-01-31 08:19:27 +0200

    chore: move weka installation script cration to deploy function

--- function-app/functions/scale_up/scale_up.go
+++ function-app/functions/scale_up/scale_up.go
@@ -3,54 +3,34 @@ package scale_up
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/rs/zerolog/log"
 	"net/http"
 	"os"
 	"weka-deployment/common"
 )
 
-type RequestBody struct {
-	Size int64 `json:"size"`
-}
-
 func Handler(w http.ResponseWriter, r *http.Request) {
-	log.Info().Msg("running scale_up")
-	clusterName := os.Getenv("CLUSTER_NAME")
+	outputs := make(map[string]interface{})
+	resData := make(map[string]interface{})
+
+	stateContainerName := os.Getenv("STATE_CONTAINER_NAME")
+	stateStorageName := os.Getenv("STATE_STORAGE_NAME")
 	subscriptionId := os.Getenv("SUBSCRIPTION_ID")
 	resourceGroupName := os.Getenv("RESOURCE_GROUP_NAME")
 	prefix := os.Getenv("PREFIX")
-
-	outputs := make(map[string]interface{})
-	resData := make(map[string]interface{})
-	var invokeRequest common.InvokeRequest
-
-	d := json.NewDecoder(r.Body)
-	err := d.Decode(&invokeRequest)
-	if err != nil {
-		log.Error().Msg("Bad request")
-		return
-	}
-
-	var reqData map[string]interface{}
-	err = json.Unmarshal(invokeRequest.Data["req"], &reqData)
-	if err != nil {
-		log.Error().Msg("Bad request")
-		return
-	}
-
-	var data RequestBody
-
-	if json.Unmarshal([]byte(reqData["Body"].(string)), &data) != nil {
-		log.Error().Msg("Bad request")
-		return
-	}
+	clusterName := os.Getenv("CLUSTER_NAME")
 
 	vmScaleSetName := fmt.Sprintf("%s-%s-vmss", prefix, clusterName)
-	err = common.UpdateVmScaleSetNum(subscriptionId, resourceGroupName, vmScaleSetName, data.Size)
+
+	state, err := common.ReadState(stateStorageName, stateContainerName)
 	if err != nil {
 		resData["body"] = err.Error()
 	} else {
-		resData["body"] = "updated size successfully"
+		err = common.UpdateVmScaleSetNum(subscriptionId, resourceGroupName, vmScaleSetName, int64(state.InitialSize))
+		if err != nil {
+			resData["body"] = err.Error()
+		} else {
+			resData["body"] = "updated size successfully"
+		}
 	}
 
 	outputs["res"] = resData
