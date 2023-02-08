c7502eea07482d7204106e9fba313fcd12f8443d
Denise Perez <denise@weka.io>
2023-02-08 15:32:11 +0200

    feat: add private example

---
+++ examples/private_network/outputs.tf
@@ -0,0 +1,16 @@
+output "get-vms-ips-command" {
+  value = module.deploy-weka.get-vms-private-ips
+}
+
+output "SSH-KEY-PATH" {
+  value = module.deploy-weka.ssh-key-files-path
+}
+
+output "DOWNLOAD-SSH-KEYS-COMMAND" {
+  value = module.deploy-weka.ssh-key-download-blob
+}
+
+output "get-cluster-status" {
+  value = module.deploy-weka.get-cluster-status
+  description = "get cluster status command"
+}
