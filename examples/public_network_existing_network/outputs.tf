8092addfef72e28d957a5d7f6148fee5b8dbb027
Denise Perez <denise@weka.io>
2023-02-09 17:57:28 +0200

    feat: Unite all outputs to one output

--- examples/public_network_existing_network/outputs.tf
+++ examples/public_network_existing_network/outputs.tf
@@ -1,16 +1,3 @@
-output "get-vms-ips-command" {
-  value = module.deploy-weka.get-vms-public-ips
-}
-
-output "SSH-KEY-PATH" {
-  value = module.deploy-weka.ssh-key-files-path
-}
-
-output "DOWNLOAD-SSH-KEYS-COMMAND" {
-  value = module.deploy-weka.ssh-key-download-blob
-}
-
-output "get-cluster-status" {
-  value = module.deploy-weka.get-cluster-status
-  description = "get cluster status command"
+output "get-cluster-helpers-commands" {
+  value = module.deploy-weka.cluster_helpers_commands
 }
