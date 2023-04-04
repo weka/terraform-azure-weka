da39df5621374ec5dc320bdac2119e0941c49750
Kristina Solovyova <kristina.solovyova@weka.io>
2023-04-04 16:37:09 +0300

    chore: add blob retention policy for 'dev' code zips

--- function-app/distribution/state.tf
+++ function-app/distribution/state.tf
@@ -2,9 +2,10 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~>3.43.0"
+      version = "~>3.50.0"
     }
   }
+  // https://learn.microsoft.com/en-us/azure/developer/terraform/store-state-in-azure-storage?tabs=azure-cli
   backend "azurerm" {
     resource_group_name  = "weka-tf-functions"
     storage_account_name = "wekatfstate"
