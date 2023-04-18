8a0eaed6de68be5ca03cec9760a4d1c731324850
denise <denise@weka.io>
2023-04-18 19:21:45 +0300

    feat: move out deploy, join and clusterize scripts generation logic

---
+++ function-app/code/lib/bash_functions/get_bash_function.go
@@ -0,0 +1,44 @@
+package bash_functions
+
+import (
+	"github.com/lithammer/dedent"
+)
+
+func GetCoreIds() string {
+	s := `
+	core_ids=$(cat /sys/devices/system/cpu/cpu*/topology/thread_siblings_list | cut -d "-" -f 1 | sort -u | tr '\n' ' ')
+	core_ids="${core_ids[@]/0}"
+	IFS=', ' read -r -a core_ids <<< "$core_ids"
+	core_idx_begin=0
+	get_core_ids() {
+		core_idx_end=$(($core_idx_begin + $1))
+		res=${core_ids[i]}
+		for (( i=$(($core_idx_begin + 1)); i<$core_idx_end; i++ ))
+		do
+			res=$res,${core_ids[i]}
+		done
+		core_idx_begin=$core_idx_end
+		eval "$2=$res"
+	}
+	`
+	return dedent.Dedent(s)
+}
+
+func GetNetStrForDpdk() string {
+	s := `
+	function getNetStrForDpdk() {
+		i=$1
+		j=$2
+		net=" "
+		gateway=$(route -n | grep 0.0.0.0 | grep UG | awk '{print $2}')
+		for ((i; i<$j; i++)); do
+			eth=$(ifconfig | grep eth$i -C2 | grep 'inet ' | awk '{print $2}')
+			enp=$(ls -l /sys/class/net/eth$i/ | grep lower | awk -F"_" '{print $2}' | awk '{print $1}')
+			bits=$(ip -o -f inet addr show eth$i | awk '{print $4}')
+			IFS='/' read -ra netmask <<< "$bits"
+			net="$net --net $enp/$eth/${netmask[1]}/$gateway"
+		done
+	}
+	`
+	return dedent.Dedent(s)
+}
---
+++ function-app/code/lib/join/join.go
@@ -0,0 +1,200 @@
+package join
+
+import (
+	"context"
+	"fmt"
+	"math/rand"
+	"strings"
+	"time"
+
+	bf "weka-deployment/lib/bash_functions"
+	fd "weka-deployment/lib/functions_def"
+
+	"github.com/lithammer/dedent"
+)
+
+func ShuffleSlice(slice []string) {
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	r.Shuffle(len(slice), func(i, j int) { slice[i], slice[j] = slice[j], slice[i] })
+}
+
+type BackendCoreCount struct {
+	Total     int
+	Frontend  int
+	Drive     int
+	Converged bool
+	Memory    int
+}
+
+type JoinParams struct {
+	VMName         string
+	IPs            []string
+	WekaUsername   string
+	WekaPassword   string
+	InstallDpdk    bool
+	NicsNum        string
+	InstanceParams BackendCoreCount
+}
+
+type JoinScriptGenerator struct {
+	FailureDomainCmd   string
+	GetInstanceNameCmd string
+	Params             JoinParams
+	FuncDef            fd.FunctionDef
+}
+
+func (j *JoinScriptGenerator) GetJoinScript(ctx context.Context) string {
+	reportFunc := j.FuncDef.GetFunctionCmdDefinition(fd.Report)
+	joinFinalizationFunc := j.FuncDef.GetFunctionCmdDefinition(fd.JoinFinalization)
+
+	getCoreIdsFunc := bf.GetCoreIds()
+	getNetStrForDpdkFunc := bf.GetNetStrForDpdk()
+
+	ips := j.Params.IPs
+	ShuffleSlice(ips)
+
+	bashScriptTemplate := `
+	#!/bin/bash
+	set -ex
+	export WEKA_USERNAME="%s"
+	export WEKA_PASSWORD="%s"
+	export WEKA_RUN_CREDS="-e WEKA_USERNAME=$WEKA_USERNAME -e WEKA_PASSWORD=$WEKA_PASSWORD"
+	IPS=(%s)
+	HASHED_IP=$(%s)
+	COMPUTE=%d
+	FRONTEND=%d
+	DRIVES=%d
+	COMPUTE_MEMORY=%d
+	INSTALL_DPDK=%t
+	NICS_NUM=%s
+	host_ips=$(IFS=, ;echo "${IPS[*]}")
+
+	declare -a backend_ips=$IPS
+
+	# report function definition
+	%s
+
+	# join_finalization function definition
+	%s
+
+	# get_core_ids bash function definition
+	%s
+
+	# getNetStrForDpdk bash function definitiion
+	%s
+
+	report -d "{\"hostname\": \"$HOSTNAME\", \"type\": \"progress\", \"message\": \"Joining started\"}"
+
+	random=$$
+	echo $random
+	for backend_ip in ${backend_ips[@]}; do
+		if VERSION=$(curl -s -XPOST --data '{"jsonrpc":"2.0", "method":"client_query_backend", "id":"'$random'"}' $backend_ip:14000/api/v1 | sed  's/.*"software_release":"\([^"]*\)".*$/\1/g'); then
+			if [[ "$VERSION" != "" ]]; then
+				break
+			fi
+		fi
+	done
+
+	ip=$(ifconfig eth0 | grep "inet " | awk '{ print $2}')
+	while [ ! $ip ] ; do
+		sleep 1
+		ip=$(ifconfig eth0 | grep "inet " | awk '{ print $2}')
+	done
+
+	report -d "{\"hostname\": \"$HOSTNAME\", \"type\": \"progress\", \"message\": \"Installing weka\"}"
+	curl $backend_ip:14000/dist/v1/install | sh
+	report -d "{\"hostname\": \"$HOSTNAME\", \"type\": \"progress\", \"message\": \"Weka installation completed\"}"
+
+	weka version get --from $backend_ip:14000 $VERSION --set-current
+	weka version prepare $VERSION
+	weka local stop && weka local rm --all -f
+
+	# weka containers setup
+
+	get_core_ids $DRIVES drive_core_ids
+	get_core_ids $COMPUTE compute_core_ids
+	get_core_ids $FRONTEND frontend_core_ids
+
+	if [[ $INSTALL_DPDK == true ]]; then
+		mgmt_ip=$(hostname -I | awk '{print $1}')
+
+		getNetStrForDpdk 1 $(($DRIVES+1))
+		sudo weka local setup container --name drives0 --base-port 14000 --cores $DRIVES --no-frontends --drives-dedicated-cores $DRIVES --join-ips $IPS --failure-domain "$HASHED_IP" --core-ids $drive_core_ids $net --management-ips $mgmt_ip --dedicate
+
+		getNetStrForDpdk $((1+$DRIVES)) $((1+$DRIVES+$COMPUTE))
+		sudo weka local setup container --name compute0 --base-port 15000 --cores $COMPUTE --memory "$COMPUTE_MEMORY"GB --no-frontends --compute-dedicated-cores $COMPUTE --join-ips $IPS --failure-domain "$HASHED_IP" --core-ids $compute_core_ids $net --management-ips $mgmt_ip --dedicate
+
+		getNetStrForDpdk $(($NICS_NUM-1)) $(($NICS_NUM))
+		sudo weka local setup container --name frontend0 --base-port 16000 --cores $FRONTEND --allow-protocols true --frontend-dedicated-cores $FRONTEND --join-ips $IPS --failure-domain "$HASHED_IP" --core-ids $frontend_core_ids $net --management-ips $mgmt_ip --dedicate
+	else
+		sudo weka local setup container --name drives0 --base-port 14000 --cores $DRIVES --no-frontends --drives-dedicated-cores $DRIVES --join-ips $host_ips --failure-domain "$HASHED_IP" --dedicate
+		sudo weka local setup container --name compute0 --base-port 15000 --cores $COMPUTE --memory "$COMPUTE_MEMORY"GB --no-frontends --compute-dedicated-cores $COMPUTE --join-ips $host_ips --failure-domain "$HASHED_IP" --dedicate
+		sudo weka local setup container --name frontend0 --base-port 16000 --cores $FRONTEND --allow-protocols true --frontend-dedicated-cores $FRONTEND --join-ips $host_ips --failure-domain "$HASHED_IP" --dedicate
+	fi
+	`
+
+	frontend := j.Params.InstanceParams.Frontend
+	drive := j.Params.InstanceParams.Drive
+	compute := j.Params.InstanceParams.Total - frontend - drive - 1
+	mem := j.Params.InstanceParams.Memory
+
+	isReady := j.getIsReadyScript()
+	addDrives := j.getAddDrivesScript()
+
+	bashScriptTemplate += isReady + addDrives
+	bashScript := fmt.Sprintf(
+		bashScriptTemplate, j.Params.WekaUsername, j.Params.WekaPassword, strings.Join(ips, " "), j.FailureDomainCmd,
+		compute, frontend, drive, mem, j.Params.InstallDpdk, j.Params.NicsNum, reportFunc, joinFinalizationFunc,
+		getCoreIdsFunc, getNetStrForDpdkFunc,
+	)
+	return dedent.Dedent(bashScript)
+}
+
+func (j *JoinScriptGenerator) getIsReadyScript() string {
+	s := `
+	while ! weka debug manhole -s 0 operational_status | grep '"is_ready": true' ; do
+		sleep 1
+	done
+	echo Connected to cluster
+	`
+	return dedent.Dedent(s)
+}
+
+func (j *JoinScriptGenerator) getAddDrivesScript() string {
+	// supposes 'report' and 'join_finalization' are already defined
+	s := `
+	compute_name=$(%s)
+
+	host_id=$(weka local run --container compute0 $WEKA_RUN_CREDS manhole getServerInfo | grep hostIdValue: | awk '{print $2}')
+	mkdir -p /opt/weka/tmp
+	cat >/opt/weka/tmp/find_drives.py <<EOL
+	import json
+	import sys
+	for d in json.load(sys.stdin)['disks']:
+		if d['isRotational']: continue
+		print(d['devPath'])
+	EOL
+	devices=$(weka local run --container compute0 $WEKA_RUN_CREDS bash -ce 'wapi machine-query-info --info-types=DISKS -J | python3 /opt/weka/tmp/find_drives.py')
+	for device in $devices; do
+		weka local exec --container drives0 /weka/tools/weka_sign_drive $device
+	done
+	ready=0
+	while [ $ready -eq 0 ] ; do
+		ready=1
+		lsblk
+		for device in $devices; do
+			if [ ! "$(lsblk | grep ${device#"/dev/"} | grep part)" ]; then
+				ready=0
+				sleep 5
+				break
+			fi
+		done
+	done
+	weka cluster drive scan $host_id
+
+	join_finalization "{\"name\": \"$compute_name\"}"
+	echo "completed successfully" > /tmp/weka_join_completion_validation
+	report "{\"hostname\": \"$HOSTNAME\", \"type\": \"progress\", \"message\": \"Join completed successfully\"}"
+	`
+	return dedent.Dedent(fmt.Sprintf(s, j.GetInstanceNameCmd))
+}
