f485f0e0b706e0a8592423c3fe04a71a7c96d7e0
Denise Perez <densie@weka.io>
2023-04-28 11:26:05 +0300

    change weka version

--- functions.tf
+++ functions.tf
@@ -120,7 +120,7 @@ resource "azurerm_linux_function_app" "function_app" {
     "SUBNETS_RANGE" = join(" ",local.subnets_range)
     "NICS_NUM" = var.container_number_map[var.instance_type].nics
     "INSTANCE_TYPE" = var.instance_type
-    "INSTALL_URL" =  var.install_weka_url != "" ? var.install_weka_url : "https://$TOKEN@get.prod.weka.io/dist/v1/install/4.2.1-46c4ca6f08f141014d0c187ec0c74d70/4.2.1.9999-24145d515c31ca74ad1337105f5a6c59"
+    "INSTALL_URL" =  var.install_weka_url != "" ? var.install_weka_url : "https://$TOKEN@get.prod.weka.io/dist/v1/install/4.2.1-2719ddc54a69c9425c81b99ceb1e94d9/4.2.1.10148-e5389916e2a2cb20d2cddf9ea532e3a6"
     "LOG_LEVEL" = var.function_app_log_level
 
     https_only = true
