9cb7a9af432d42f627e61814f63e3401dea0eb1d
Kristina Solovyova <kristina.solovyova@weka.io>
2023-05-11 16:29:46 +0300

    chore: add Taskfile for function app zip dist, remove old tf files
    - add Taskfile to build and upload function app zip
    - remove upload_zip tf module
    - use task in github actions distribution workflow

--- function-app/distribution/variables.tf
+++ function-app/distribution/variables.tf
@@ -9,47 +9,8 @@ variable "subscription_id" {
   description = "Subscription id for deployment"
 }
 
-variable "dist" {
+variable "supported_regions_file_path" {
   type        = string
-  description = "Distribution option ('dev' or 'release')"
-  default     = "dev"
-
-  validation {
-    condition     = contains(["dev", "release"], var.dist)
-    error_message = "Valid value is one of the following: dev, release."
-  }
-}
-
-variable "regions" {
-  type        = map(list(string))
-  description = "Map of lists of supported regions"
-
-  default = {
-    "dev" = ["eastus"],
-    "release" = [
-      "brazilsouth",
-      "canadacentral",
-      "centralus",
-      "eastus",
-      "eastus2",
-      //"northcentralus", // does not include the Zone-redundant storage (ZRS) blob
-      "southcentralus",
-      //"westus", // Sku: Standard_ZRS, Kind: StorageV2 is not available in zone
-      "westus2",
-      "westus3",
-      "francecentral",
-      "germanywestcentral",
-      "northeurope",
-      "westeurope",
-      "uksouth",
-      "swedencentral",
-      "qatarcentral",
-      "australiaeast",
-      //"australiasoutheast", // does not include the Zone-redundant storage (ZRS) blob
-      "centralindia",
-      "japaneast",
-      "eastasia",
-      "southeastasia",
-    ]
-  }
+  description = "Path to supported regions file"
+  default     = "supported_regions/release.txt"
 }
