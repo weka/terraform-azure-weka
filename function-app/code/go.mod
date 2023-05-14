5600b25dc1df249eb7d3d4a706ef112e965b586b
Assaf Giladi <assaf@weka.io>
2023-05-14 20:11:09 +0300

    fix: azure private installation script

--- function-app/code/go.mod
+++ function-app/code/go.mod
@@ -13,7 +13,7 @@ require (
 	github.com/Azure/azure-sdk-for-go/sdk/storage/azblob v1.0.0
 	github.com/google/uuid v1.3.0
 	github.com/lithammer/dedent v1.1.0
-	github.com/weka/go-cloud-lib v0.0.0-20230510123910-196de4464ee6
+	github.com/weka/go-cloud-lib v0.0.0-20230514161451-11bffc5ae05b
 )
 
 require (
--- variables.tf
+++ variables.tf
@@ -295,7 +295,7 @@ variable "function_app_storage_account_container_prefix" {
 variable "function_app_version" {
   type = string
   description = "Function app code version (hash)"
-  default = "b6686329458fd04eaeac2340cf1fe42f"
+  default = "6506b06fba47033e4cf55820c11b8f95"
 }
 
 variable "function_app_dist" {
