8ecfdfd81501b6098afb4003a749c914445d0061
Kristina Solovyova <kristina.solovyova@weka.io>
2023-05-15 20:24:13 +0300

    chore: fix files sort order when calculating md5 of the dir

--- zip_function_app_creation/get_function_app_hash.sh
+++ zip_function_app_creation/get_function_app_hash.sh
@@ -10,8 +10,8 @@ function_code_path="$2"
 go mod tidy > /dev/null 2>&1
 
 if [ $os_name == "darwin" ]; then
-    function_app_code_hash="$(find ${function_code_path} -type f -print0 | sort -z | xargs -0 md5 | awk {'print $NF'} ORS='' | md5)"
+    function_app_code_hash="$(find ${function_code_path} -type f | LC_ALL=C sort | xargs -n1 md5 | awk {'print $NF'} ORS='' | md5)"
 else
-    function_app_code_hash="$(find ${function_code_path} -type f -print0 | sort -z | xargs -0 md5sum | awk '{print $1}' ORS='' | md5sum | awk '{print $1}')"
+    function_app_code_hash="$(find ${function_code_path} -type f | LC_ALL=C sort | xargs -n1 md5sum | awk '{print $1}' ORS='' | md5sum | awk '{print $1}')"
 fi
 echo $function_app_code_hash
