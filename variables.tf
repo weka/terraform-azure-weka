fbcc34f88c48df8aa0fcfc1d7c4c82ecf6219204
Assaf Giladi <assaf@weka.io>
2023-05-18 17:08:39 +0300

    fix: update go-cloud-lib: join fix

--- variables.tf
+++ variables.tf
@@ -301,7 +301,7 @@ variable "function_app_storage_account_container_prefix" {
 variable "function_app_version" {
   type = string
   description = "Function app code version (hash)"
-  default = "7429d8d378635959444c6ba7bc74f96d"
+  default = "fff80ad03d66b751794b69a325c75736"
 }
 
 variable "function_app_dist" {
