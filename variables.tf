92cb412969fcf098f83d45542e650ff600c23034
Assaf Giladi <assaf@weka.io>
2023-05-22 17:23:12 +0300

    fix: update go-cloud-lib package with scale down fixes

--- variables.tf
+++ variables.tf
@@ -301,7 +301,7 @@ variable "function_app_storage_account_container_prefix" {
 variable "function_app_version" {
   type = string
   description = "Function app code version (hash)"
-  default = "916104b9989625be6f60de43aef19b0c"
+  default = "525a5fc3040aeeff2ce27e6ffd39bd14"
 }
 
 variable "function_app_dist" {
