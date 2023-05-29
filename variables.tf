c599452d57b5a4b7c0fb2bfea67b247c99acd03f
Assaf Giladi <assaf@weka.io>
2023-05-29 18:24:58 +0300

    chore: update weka default version 4.2.0.120-beta

--- variables.tf
+++ variables.tf
@@ -77,7 +77,7 @@ variable "subnet_delegation" {
 variable "weka_version" {
   type = string
   description = "The Weka version to deploy."
-  default = "4.2.0.86-beta"
+  default = "4.2.0.120-beta"
 }
 
 variable "get_weka_io_token" {
