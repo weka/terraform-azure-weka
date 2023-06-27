950a649d4d417e62a7e67dfb80ea1e23494abd92
Denise Perez <densie@weka.io>
2023-06-27 14:12:49 +0300

    feat: add create client module

---
+++ clients.tf
@@ -0,0 +1,47 @@
+resource "null_resource" "get-backend-ip" {
+  count = var.clients_number > 0 ? 1 : 0
+  triggers = {
+    always_run = timestamp()
+  }
+  provisioner "local-exec" {
+    command = "az vmss nic list -g ${var.rg_name} --vmss-name ${azurerm_linux_virtual_machine_scale_set.vmss.name} --subscription ${var.subscription_id} --query \"[].ipConfigurations[]\" | jq -r '.[] | select(.name==\"ipconfig0\")'.privateIPAddress > ${path.root}/backend_ips"
+  }
+  depends_on = [azurerm_linux_virtual_machine_scale_set.vmss]
+}
+
+data "local_file" "backend_ips" {
+  filename = "${path.root}/backend_ips"
+  depends_on = [null_resource.get-backend-ip]
+}
+
+module "clients" {
+  count              = var.clients_number > 0 ? 1 : 0
+  source             = "./modules/clients"
+  rg_name            = var.rg_name
+  clients_name       = "${var.prefix}-${var.cluster_name}-client"
+  clients_number     = var.clients_number
+  mount_clients_dpdk = var.mount_clients_dpdk
+  subnet_name        = var.subnet_name
+  apt_repo_url       = var.apt_repo_url
+  vnet_name          = var.vnet_name
+  nics               = var.mount_clients_dpdk ? var.client_nics_num : 1
+  instance_type      = var.client_instance_type
+  backend_ips        = [replace(join(" ",[data.local_file.backend_ips.content]), "\n", " ")]
+  ssh_public_key     = var.ssh_public_key == null ? tls_private_key.ssh_key[0].public_key_openssh : var.ssh_public_key
+  ppg_id             = azurerm_proximity_placement_group.ppg.id
+  assign_public_ip   = var.private_network ? false : true
+  vnet_rg_name       = var.vnet_rg_name
+  source_image_id    = var.source_image_id
+  sg_id              = var.sg_id
+}
+
+resource "null_resource" "clean" {
+  count = var.clients_number > 0 ? 1 : 0
+  triggers = {
+    always_run = timestamp()
+  }
+  provisioner "local-exec" {
+    command = "rm -f ${path.root}/backend_ips"
+  }
+  depends_on = [module.clients]
+}
---
+++ modules/clients/clients.tf
@@ -0,0 +1,110 @@
+data "azurerm_resource_group" "rg" {
+  name = var.rg_name
+}
+
+data "azurerm_subnet" "subnet" {
+  resource_group_name  = var.vnet_rg_name
+  virtual_network_name = var.vnet_name
+  name                 = var.subnet_name
+}
+
+locals {
+  secondary_nics_num      = (var.nics - 1) * var.clients_number
+  private_nic_first_index = var.assign_public_ip ? 1 : 0
+  preparation_script      = templatefile("${path.module}/init.sh", {
+    apt_repo_url  = var.apt_repo_url
+    nics_num      = var.nics
+    install_dpdk  = var.mount_clients_dpdk
+    subnet_range  = data.azurerm_subnet.subnet.address_prefix
+  })
+
+  mount_wekafs_script = templatefile("${path.module}/mount_wekafs.sh", {
+    all_subnets = split("/", data.azurerm_subnet.subnet.address_prefix)[0]
+    all_gateways = cidrhost(data.azurerm_subnet.subnet.address_prefix, 1)
+    nics_num           = var.nics
+    backend_ips        = join(" ", var.backend_ips)
+    mount_clients_dpdk = var.mount_clients_dpdk
+  })
+
+  custom_data_parts = [local.preparation_script, local.mount_wekafs_script]
+  vms_custom_data   = base64encode(join("\n", local.custom_data_parts))
+}
+
+resource "azurerm_linux_virtual_machine_scale_set" "vmss" {
+  name                            = "${var.clients_name}-vmss"
+  location                        = data.azurerm_resource_group.rg.location
+  resource_group_name             = var.rg_name
+  sku                             = var.instance_type
+  upgrade_mode                    = "Manual"
+  admin_username                  = var.vm_username
+  instances                       = var.clients_number
+  computer_name_prefix            = "${var.clients_name}-vmss"
+  custom_data                     = local.vms_custom_data
+  disable_password_authentication = true
+  proximity_placement_group_id    = var.ppg_id
+  tags                            = merge( {"weka_cluster": var.clients_name})
+  source_image_id                 = var.source_image_id
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "StandardSSD_LRS"
+  }
+
+  admin_ssh_key {
+    username   = var.vm_username
+    public_key = var.ssh_public_key
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  dynamic "network_interface" {
+    for_each = range(local.private_nic_first_index)
+    content {
+      name                          = "${var.clients_name}-nic-${network_interface.value}"
+      network_security_group_id     = var.sg_id
+      primary                       = true
+      enable_accelerated_networking = true
+      ip_configuration {
+        primary                                = true
+        name                                   = "ipconfig0"
+        subnet_id                              = data.azurerm_subnet.subnet.id
+        public_ip_address {
+          name = "${var.clients_name}-public-ip"
+        }
+      }
+    }
+  }
+  dynamic "network_interface" {
+    for_each = range(local.private_nic_first_index, 1)
+    content {
+      name                          = "${var.clients_name}-nic-${network_interface.value}"
+      network_security_group_id     = var.sg_id
+      primary                       = true
+      enable_accelerated_networking = true
+      ip_configuration {
+        primary    = true
+        name       = "ipconfig0"
+        subnet_id  = data.azurerm_subnet.subnet.id
+      }
+    }
+  }
+  dynamic "network_interface" {
+    for_each = range(1, local.secondary_nics_num)
+    content {
+      name                          = "${var.clients_name}-nic-${network_interface.value}"
+      network_security_group_id     = var.sg_id
+      primary                       = false
+      enable_accelerated_networking = true
+      ip_configuration {
+        primary       = true
+        name          = "ipconfig-${network_interface.value}"
+        subnet_id     = data.azurerm_subnet.subnet.id
+      }
+    }
+  }
+  lifecycle {
+    ignore_changes = [ instances, custom_data ]
+  }
+}
