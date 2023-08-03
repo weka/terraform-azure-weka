69e66885ce7a6537f0a82d64d00508972e08d521
Assaf Giladi <assaf@weka.io>
2023-08-03 08:52:16 +0300

    chore: update default weka version 4.2.1

--- zip_function_app_creation/get_function_app_hash.sh
+++ zip_function_app_creation/get_function_app_hash.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
-
 # Usage:
 # get_function_app_hash.sh <os_name> <function_code_path>
+
+set -e
 
 os_name="$1"
 function_code_path="$2"
 
 # make sure go.mod and go.sum are up-to-date
+cd "$function_code_path"
+rm -rf weka-deployment # remove local build in case it exists
 go mod tidy > /dev/null 2>&1
 
 if [ $os_name == "darwin" ]; then
