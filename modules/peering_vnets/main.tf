edb649391169deba32fe266c65f564e55037aecd
Assaf Giladi <assaf@weka.io>
2023-09-13 23:42:40 +0300

    fix: vnet peering module usage

--- modules/peering_vnets/main.tf
+++ modules/peering_vnets/main.tf
@@ -1,6 +1,6 @@
 data azurerm_virtual_network "vnet_id" {
   name = var.vnet_name
-  resource_group_name = var.rg_name
+  resource_group_name = var.vnet_rg_name
 }
 
 data azurerm_virtual_network "remote_vnet_ids" {
@@ -12,7 +12,7 @@ data azurerm_virtual_network "remote_vnet_ids" {
 resource "azurerm_virtual_network_peering" "peering" {
   count                        = length(var.vnet_to_peering)
   name                         = "${var.vnet_name}-peering-to-${var.vnet_to_peering[count.index]["vnet"]}"
-  resource_group_name          = var.rg_name
+  resource_group_name          = var.vnet_rg_name
   virtual_network_name         = var.vnet_name
   allow_virtual_network_access = true
   remote_virtual_network_id    = data.azurerm_virtual_network.remote_vnet_ids[count.index].id
