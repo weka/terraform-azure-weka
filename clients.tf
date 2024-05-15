58900dc52a461e71b03c7248930ed8c1d2572f70
Ihor Weka <ihor.riabov@weka.io>
2024-05-15 10:39:55 +0200

    feat: updated the arch logic in client module

--- clients.tf
+++ clients.tf
@@ -1,11 +1,3 @@
-locals {
-  default_client_instance_type = {
-    x86_64 = "Standard_D8_v5"
-    arm64  = "Standard_E2ps_v5"
-  }
-}
-
-
 module "clients" {
   count                        = var.clients_number > 0 ? 1 : 0
   source                       = "./modules/clients"
@@ -18,16 +10,17 @@ module "clients" {
   vnet_name                    = local.vnet_name
   vm_identity_name             = var.client_identity_name
   frontend_container_cores_num = var.clients_use_dpdk ? var.client_frontend_cores : 1
-  instance_type                = var.client_instance_type != "" ? var.client_instance_type : local.default_client_instance_type[var.client_arch]
+  instance_type                = var.client_instance_type
   backend_lb_ip                = var.create_lb ? azurerm_lb.backend_lb[0].private_ip_address : ""
   ssh_public_key               = var.ssh_public_key == null ? tls_private_key.ssh_key[0].public_key_openssh : var.ssh_public_key
   ppg_id                       = var.client_placement_group_id == "" ? local.placement_group_id : var.client_placement_group_id
   assign_public_ip             = local.assign_public_ip
   vnet_rg_name                 = local.vnet_rg_name
-  source_image_id              = var.client_source_image_id[var.client_arch]
+  source_image_id              = var.client_source_image_id
   sg_id                        = local.sg_id
   tags_map                     = var.tags_map
   custom_data                  = var.clients_custom_data
   vmss_name                    = "${var.prefix}-${var.cluster_name}-vmss"
   depends_on                   = [azurerm_proximity_placement_group.ppg, module.network]
+  arch                         = var.client_arch
 }
