cc92a0e2ca940a05f921625a2aa2b82e76ae264a
Kristina Solovyova <kristina.solovyova@weka.io>
2024-11-06 14:02:02 +0200

    fix: protocol gw dynamic public ip allocation

--- modules/protocol_gateways/main.tf
+++ modules/protocol_gateways/main.tf
@@ -16,6 +16,7 @@ resource "azurerm_public_ip" "this" {
   location            = var.location
   resource_group_name = var.rg_name
   allocation_method   = "Dynamic"
+  sku                 = "Basic"
 }
 
 resource "azurerm_network_interface" "primary_gateway_nic_public" {
