11e4728a7628aa105a536ba5b77165bed19c09bb
Assaf Giladi <assaf@weka.io>
2025-04-10 12:30:56 +0300

    chore: update management ip fetch

--- modules/protocol_gateways/setup_s3.sh
+++ modules/protocol_gateways/setup_s3.sh
@@ -23,7 +23,7 @@ function retry_create_s3_cluster {
   msg="S3 cluster is created"
   check_cluster_status
   while [ $count -gt 0 ]; do
-      weka s3 cluster create $filesystem_name .config_fs --container $all_container_ids_str --port 9000 && break
+      weka s3 cluster create default .config_fs --container $all_container_ids_str --port 9000 && break
       count=$(($count - 1))
       echo "Retrying create S3 cluster in $retry_sleep seconds..."
       report "{\"hostname\": \"$HOSTNAME\", \"type\": \"progress\", \"message\": \"$msg\"}"
