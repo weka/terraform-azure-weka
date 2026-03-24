f897a968789fe93d9d6c6ff78ac44128d0b6296f
Assaf Giladi <assaf@weka.io>
2026-03-24 07:46:21 +0200

    fix: update go cloud lib library: retry to fetch full capacity

--- function-app/code/go.mod
+++ function-app/code/go.mod
@@ -14,7 +14,7 @@ require (
 	github.com/google/go-cmp v0.6.0
 	github.com/google/uuid v1.6.0
 	github.com/lithammer/dedent v1.1.0
-	github.com/weka/go-cloud-lib v0.0.0-20260130121629-d14c8f82fcd0
+	github.com/weka/go-cloud-lib v0.0.0-20260326083808-496b49efded3
 )
 
 require (
--- variables.tf
+++ variables.tf
@@ -410,13 +410,13 @@ variable "function_app_storage_account_container_prefix" {
 variable "function_app_version" {
   type        = string
   description = "Function app code version (hash)"
-  default     = "f3ab109f5228ce3b2a5680a5cccdfddf"
+  default     = "cfcfae26fee29902ed6e4dde17762be4"
 }
 
 variable "function_app_dist" {
   type        = string
   description = "Function app code dist"
-  default     = "release"
+  default     = "dev"
 
   validation {
     condition     = contains(["dev", "release"], var.function_app_dist)
