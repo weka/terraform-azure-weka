a48e0341957a5a586d995cef521069dc5ff8e790
Assaf Giladi <assaf@weka.io>
2026-07-21 21:20:32 +0300

    fix: update protocol gws cli parsing

--- function-app/code/go.mod
+++ function-app/code/go.mod
@@ -14,7 +14,7 @@ require (
 	github.com/google/go-cmp v0.6.0
 	github.com/google/uuid v1.6.0
 	github.com/lithammer/dedent v1.1.0
-	github.com/weka/go-cloud-lib v0.0.0-20260712093451-70533568df2f
+	github.com/weka/go-cloud-lib v0.0.0-20260720055256-2878cba391ba
 )
 
 require (
--- variables.tf
+++ variables.tf
@@ -484,7 +484,7 @@ variable "function_app_storage_account_container_prefix" {
 variable "function_app_version" {
   type        = string
   description = "Function app code version (hash)"
-  default     = "03b41d523a82e97625893b63dbad6294"
+  default     = "7149806b3db54203e017269912bb541d"
 }
 
 variable "function_app_dist" {
