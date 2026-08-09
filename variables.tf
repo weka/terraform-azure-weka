8365e3ced090345551b4716ccc258ccea7c07b40
Assaf Giladi <assaf@weka.io>
2026-08-09 11:40:45 +0300

    Revert "fix: use legacy pool auto behavior"
    This reverts commit 2065692756bedf237ab796995b7366c4a2ce103d.

--- variables.tf
+++ variables.tf
@@ -484,7 +484,7 @@ variable "function_app_storage_account_container_prefix" {
 variable "function_app_version" {
   type        = string
   description = "Function app code version (hash)"
-  default     = "5bd696742ee1b211a0025a8ba4f87f94"
+  default     = "7149806b3db54203e017269912bb541d"
 }
 
 variable "function_app_dist" {
